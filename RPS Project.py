22cb34ffdef27ca015f7c8a6fcd36c76786299f0
Cole Riche <121520130+frogcarrot@users.noreply.github.com>
2023-01-02 15:02:34 -0800

    Update RPS Project.py

--- RPS Project.py
+++ RPS Project.py
@@ -1,4 +1,4 @@
-def game(user_pick, computer_pick, os, user_name, random):
+def game(user_pick, computer_pick, os, user_name, random, wins, losses):
 
     # Sets up responses for every result
 
@@ -10,22 +10,27 @@ def game(user_pick, computer_pick, os, user_name, random):
 
     elif user_pick.title() == "S":
         user_pick = "scissors"
+    
+    elif user_pick.title() == "Q":
+        print("\nGoodbye!") 
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        exit()
 
-    # Response for an unknown input
+	# Response for an unknown input
 	
     else: 
         os.system('cls')
         print("Enter a real option, you fucking pussy.")
         input("\nPress enter to continue...")
         os.system('cls')
-        user_pick = input("Enter (R)ock, (P)aper, or (S)cissors: ")
-        game(user_pick, computer_pick, os, user_name, random)
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
 
-    # Shows what the player chose
-	
+	# Shows what the player chose
+		
     print("\n" + user_name + " chose " + str(user_pick) + "!")
 	
-    # Sets the computer's number choice to rock, paper, or scissors 
+	# Sets the computer's number choice to rock, paper, or scissors 
 	
     if computer_pick == 1:
         computer_pick = "rock"
@@ -47,72 +52,102 @@ def game(user_pick, computer_pick, os, user_name, random):
     if user_pick == "rock" and computer_pick == "rock":
         os.system('cls')
         print("Tie game!")
+        print("\nTry another choice.")
         input("\nPress enter to continue...")
         os.system('cls')
-        print("Try another choice.")
-        input("\nPress enter to continue...")
-        os.system('cls')
-        user_pick = input("Enter (R)ock, (P)aper, or (S)cissors: ")
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
         computer_pick = random.randint(1, 3)
-        game(user_pick, computer_pick, os, user_name, random)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
 
     if user_pick == "rock" and computer_pick == "paper":
         os.system('cls')
         print("The computer wins!")
-        exit()
+        losses += 1
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        input("\nPress enter to continue...")
+        os.system('cls')
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        computer_pick = random.randint(1, 3)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
         
     if user_pick == "rock" and computer_pick == "scissors":
         os.system('cls')
         print(str(user_name) + " wins!")
-        exit()
-        
+        wins += 1
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        input("\nPress enter to continue...")
+        os.system('cls')
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        computer_pick = random.randint(1, 3)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
+         
     # Paper responses
     
     if user_pick == "paper" and computer_pick == "rock":
         os.system('cls')
         print(str(user_name) + " wins!")
-        exit()
-
+        wins += 1
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        input("\nPress enter to continue...")
+        os.system('cls')
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        computer_pick = random.randint(1, 3)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
+        
     if user_pick == "paper" and computer_pick == "paper":
         os.system('cls')
         print("Tie game!")
+        print("\nTry another choice.")
         input("\nPress enter to continue...")
         os.system('cls')
-        print("Try another choice.")
-        input("\nPress enter to continue...")
-        os.system('cls')
-        user_pick = input("Enter (R)ock, (P)aper, or (S)cissors: ")
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
         computer_pick = random.randint(1, 3)
-        game(user_pick, computer_pick, os, user_name, random)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
         
     if user_pick == "paper" and computer_pick == "scissors":
         os.system('cls')
         print("The computer wins!")
-        exit()
+        losses += 1        
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        input("\nPress enter to continue...")
+        os.system('cls')
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        computer_pick = random.randint(1, 3)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
         
     # Scissors responses
     
     if user_pick == "scissors" and computer_pick == "rock":
         os.system('cls')
         print("The computer wins!")
-        exit()
+        losses += 1
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        input("\nPress enter to continue...")
+        os.system('cls')
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        computer_pick = random.randint(1, 3)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
 
     if user_pick == "scissors" and computer_pick == "paper":
         os.system('cls')
         print(str(user_name) + " wins!")
-        exit()
+        wins += 1
+        print("\nWins: " + str(wins) + "\nLosses: " + str(losses))
+        input("\nPress enter to continue...")
+        os.system('cls')
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+        computer_pick = random.randint(1, 3)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
         
     if user_pick == "scissors" and computer_pick == "scissors":
         os.system('cls')
         print("Tie game!")
+        print("\nTry another choice.")
         input("\nPress enter to continue...")
         os.system('cls')
-        print("Try another choice.")
-        input("\nPress enter to continue...")
-        os.system('cls')
-        user_pick = input("Enter (R)ock, (P)aper, or (S)cissors: ")
+        user_pick = input("Exit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
         computer_pick = random.randint(1, 3)
-        game(user_pick, computer_pick, os, user_name, random)
+        game(user_pick, computer_pick, os, user_name, random, wins, losses)
     
 def main():
 
@@ -128,9 +163,11 @@ def main():
 
     # Defines variables in game()
     user_name = input("Enter your name: ")
-    user_pick = input("\nEnter (R)ock, (P)aper, or (S)cissors: ")
-    computer_pick = random.randint(1, 3)
-    user_pick = game(user_pick, computer_pick, os, user_name, random)
+    user_pick = input("\nExit with (Q)uit, or Enter (R)ock, (P)aper, or (S)cissors: ")
+    wins = 0
+    losses = 0
+    computer_pick = random.randint(1, 3) 
+    user_pick = game(user_pick, computer_pick, os, user_name, random, wins, losses)
 
 main()
-game()
+game(user_pick, computer_pick, os, user_name, random, wins, losses)
